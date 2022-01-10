d3d673ee9ab7fe6f71b7f2bd07ba217683480e34
panviktar <panviktar@gmail.com>
2022-01-10 21:31:02 +0300

    ref: optimize section about-me__project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # https://panviktar.github.io/rsschool-cv/cv
+
 # https://panviktar.github.io/rsschool-cv/
--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // Selection of HTML objects
-const burger = document.querySelector('.burger i');
-const nav = document.querySelector('.nav');
+const burger = document.querySelector(".burger i");
+const nav = document.querySelector(".nav");
 
 // Defining a function
 function toggleNav() {
-    burger.classList.toggle('fa-bars');
-    burger.classList.toggle('fa-times');
-    nav.classList.toggle('nav-active');
+  burger.classList.toggle("fa-bars");
+  burger.classList.toggle("fa-times");
+  nav.classList.toggle("nav-active");
 }
 
 // Calling the function after click event occurs
-burger.addEventListener('click', function() {
-    toggleNav();
+burger.addEventListener("click", function () {
+  toggleNav();
 });
